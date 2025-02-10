648ae7861ed214e09e7832d937cc4d350a555ffb
강주현 <juhkang@gangjuhyeon-ui-MacBookPro.local>
2025-02-10 20:00:51 +0900

    토큰 검사 및 자동로그인

--- src/navigations/root/RootNavigator.tsx
+++ src/navigations/root/RootNavigator.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, {useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useAuth} from '@/utils/AuthContext';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
+import AuthStackNavigator from '../stack/AuthStackNavigator';
+import {getEncryptStorage} from '@/utils/EncryptStorage';
+import {storageKeys} from '@/constants/keys';
+import axiosInstance from '@/api/axios';
 
 const Stack = createStackNavigator();
 
 const RootNavigator = () => {
   const {isLogin, setIsLogin} = useAuth();
 
-  return isLogin ? <Text>로그인 성공</Text> : <Text>로그인 실패</Text>;
+  const refreshAuth = async () => {
+    const refreshToken = await getEncryptStorage(storageKeys.REFRESH_TOKEN);
+    if (!refreshToken) {
+      offLogin();
+      return;
+    }
+    const accessToken = await getEncryptStorage(storageKeys.ACCESS_TOKEN);
+    const sendFormData = new FormData();
+    sendFormData.append('refreshToken', refreshToken);
+    sendFormData.append('accessToken', accessToken);
+    const response = await axiosInstance.post('/auth/refresh', sendFormData);
+    if (response.status === 200) {
+      onLogin();
+    } else {
+      offLogin();
+    }
+  };
+  const onLogin = () => {
+    setIsLogin(true);
+  };
+  const offLogin = () => {
+    setIsLogin(false);
+  };
+
+  useEffect(() => {
+    refreshAuth();
+  }, []);
+
+  return isLogin ? <Text>로그인 성공</Text> : <AuthStackNavigator />;
 };
 
 export default RootNavigator;
