37161f59e2b392d726b773cb32cf1d01202f8b82
강주현 <juhkang@gangjuhyeon-ui-MacBookPro.local>
2025-02-10 16:15:55 +0900

    Auth 네비게이터 설정

---
+++ src/navigations/stack/AuthStackNavigator.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {useAuth} from '@/utils/AuthContext';
+
+import AuthHomeScreen from '@/screens/auth/AuthHomeScreen';
+import AuthLoginScreen from '@/screens/auth/AuthLoginScreen';
+import AuthSignupScreen from '@/screens/auth/AuthSignupScreen';
+import {authNavigations} from '@/constants';
+
+export type AuthStackParamList = {
+  [authNavigations.AUTH_HOME]: undefined;
+  [authNavigations.AUTH_LOGIN]: undefined;
+  [authNavigations.AUTH_SIGNUP]: undefined;
+};
+
+function AuthStackNavigator() {
+  return (
+    <Stack.Navigator initialRouteName={authNavigations.AUTH_HOME}>
+      <Stack.Screen
+        name={authNavigations.AUTH_HOME}
+        component={AuthHomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name={authNavigations.AUTH_LOGIN}
+        component={AuthLoginScreen}
+        options={{headerShown: true}}
+      />
+      <Stack.Screen
+        name={authNavigations.AUTH_SIGNUP}
+        component={AuthSignupScreen}
+        options={{headerShown: true}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const Stack = createStackNavigator();
