3a90046e7b2a5e94ed360924eb92639b7bac8ba4
juhkang <juhyunk0820@naver.com>
2025-02-12 00:04:47 +0900

    mainTab 예시 스크린 구현(삭제예정)

---
+++ src/screens/mainTab/tabHomeStack/tabHomeStackHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function TabHomeStackHomeScreen() {
+  return (
+    <View>
+      <Text>TabHomeStackHomeScreen</Text>
+    </View>
+  );
+}
+
+export default TabHomeStackHomeScreen;
---
+++ src/screens/mainTab/tabSearchStack/tabSearchStackHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function TabSearchStackHomeScreen() {
+  return (
+    <View>
+      <Text>TabSearchStackHomeScreen</Text>
+    </View>
+  );
+}
+
+export default TabSearchStackHomeScreen;
