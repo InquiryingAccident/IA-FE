5660df76322b7ee8aad2b161718b2aa308f2cf69
juhkang <juhyunk0820@naver.com>
2025-02-12 00:08:15 +0900

    이름 변경

---
+++ src/screens/mainTab/tabHomeStack/TabHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function TabHomeScreen() {
+  return (
+    <View>
+      <Text>TabHomeScreen</Text>
+    </View>
+  );
+}
+
+export default TabHomeScreen;
---
+++ src/screens/mainTab/tabSearchStack/TabSearchHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function tabSearchHomeScreen() {
+  return (
+    <View>
+      <Text>tabSearchHomeScreen</Text>
+    </View>
+  );
+}
+
+export default tabSearchHomeScreen;
