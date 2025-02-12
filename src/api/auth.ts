2fc2b7373fc996a424d5bf9697532af7d383a361
juhkang <juhyunk0820@naver.com>
2025-02-12 23:41:11 +0900

    로그인,회원가입 api구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,23 +6,42 @@ import {
 import {User} from '@/types/user';
 import axiosInstance from './axios';
 
+type RequestUser = {
+  email: string;
+  password: string;
+};
+
+type RequestSignupUser = {
+  email: string;
+  password: string;
+  nickname: string;
+};
+
+type ResponseSignupUser = {};
+
 type AuthToken = {
   accessToken: string;
   refreshToken: string;
 };
 
-
 // 서새찬 미친놈
 // //모든 api formData 형식, post
-const authLogin = async () => {};
-
-const authSignup = async (formDataFile: FormData) => {
-    const accessToken = getAccessToken();
-    const response = axiosInstance.post('/api/auth/signup', formDataFile, headers:{
-        Authorization: `Bearer ${accessToken}`
-    })
+const postLogin = async ({
+  email,
+  password,
+}: RequestUser): Promise<AuthToken> => {
+  const formData = new FormData();
+  formData.append('email', email);
+  formData.append('password', password);
+  const response = await axiosInstance.post('/api/auth/login', formData);
+  return JSON.parse(response.data);
 };
 
-const refreshToken = async ({
-  refreshToken,
-}: AuthToken): Promise<AuthToken> => {};
+const postSignup = async ({email, password, nickname}: RequestSignupUser) => {
+  const formData = new FormData();
+  formData.append('email', email);
+  formData.append('password', password);
+  formData.append('nickname', nickname);
+  const response = await axiosInstance.post('/api/auth/signup', formData);
+  return JSON.parse(response.data);
+};
