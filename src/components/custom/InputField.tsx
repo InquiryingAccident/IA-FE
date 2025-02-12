eb729ba266f9b834d2ae1f5c98f121410b7c36ad
juhkang <juhyunk0820@naver.com>
2025-02-12 23:41:35 +0900

    InputField 수정

--- src/components/custom/InputField.tsx
+++ src/components/custom/InputField.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import {StyleSheet, TextInputProps} from 'react-native';
+import React, {ForwardedRef, forwardRef, useRef} from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  TextInput,
+  View,
+  TextInputProps,
+  Text,
+  Pressable,
+} from 'react-native';
+
+import {mergeRefs} from '@/utils';
+import {colors} from '@/constants';
 
 interface InputFieldProps extends TextInputProps {
   disabled?: boolean;
@@ -7,8 +18,70 @@ interface InputFieldProps extends TextInputProps {
   touched?: boolean;
 }
 
-function InputField() {}
+const deviceHeight = Dimensions.get('screen').height;
+
+const InputField = forwardRef(
+  (
+    {disabled = false, error, touched, ...props}: InputFieldProps,
+    ref?: ForwardedRef<TextInput>,
+  ) => {
+    const innerRef = useRef<TextInput | null>(null);
+
+    const handlePressInput = () => {
+      innerRef.current?.focus();
+    };
+
+    return (
+      <Pressable onPress={handlePressInput}>
+        <View
+          style={[
+            styles.container,
+            disabled && styles.disabled,
+            touched && Boolean(error) && styles.inputError,
+          ]}>
+          <TextInput
+            ref={ref ? mergeRefs(innerRef, ref) : innerRef}
+            editable={!disabled}
+            placeholderTextColor={colors.GRAY_500}
+            style={[styles.input, disabled && styles.disabled]}
+            autoCapitalize="none"
+            spellCheck={false}
+            autoCorrect={false}
+            {...props}
+          />
+          {touched && Boolean(error) && (
+            <Text style={styles.error}>{error}</Text>
+          )}
+        </View>
+      </Pressable>
+    );
+  },
+);
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    padding: deviceHeight > 700 ? 15 : 10,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.BLACK,
+    padding: 0,
+  },
+  disabled: {
+    backgroundColor: colors.GRAY_200,
+    color: colors.GRAY_700,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.RED_300,
+  },
+  error: {
+    color: colors.RED_500,
+    fontSize: 12,
+    paddingTop: 5,
+  },
+});
 
 export default InputField;
-
-const styles = StyleSheet.create({});
