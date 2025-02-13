7eb90a0dccc8f20e6c0d29f2747b58db5776b01c
juhkang <juhyunk0820@naver.com>
2025-02-13 23:37:24 +0900

    도메인 타입 설정 - 유저관련

---
+++ src/types/domain.ts
@@ -0,0 +1,10 @@
+interface Profile {
+  memberId: string;
+  email: string;
+  nickname: string;
+  accountStatus: string;
+  createDate: string;
+  lastLonginTime: string;
+}
+
+export type {Profile};
