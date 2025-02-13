77a38648e498b8fc47b2900f913bd65e606ecbd7
juhkang <juhyunk0820@naver.com>
2025-02-14 00:13:56 +0900

    Ref 적용

--- src/screens/auth/AuthSignupScreen.tsx
+++ src/screens/auth/AuthSignupScreen.tsx
@@ -1,8 +1,13 @@
+import CustomButton from '@/components/custom/CustomButton';
+import InputField from '@/components/custom/InputField';
 import {authNavigations} from '@/constants';
+import useAuth from '@/hooks/queries/useAuth';
+import useForm from '@/hooks/useForm';
 import {AuthStackParamList} from '@/navigations/stack/AuthStackNavigator';
+import {validateSignup} from '@/utils';
 import {StackScreenProps} from '@react-navigation/stack';
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, {useRef} from 'react';
+import {View, Text, SafeAreaView, StyleSheet, TextInput} from 'react-native';
 
 type AuthScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -10,13 +15,89 @@ type AuthScreenProps = StackScreenProps<
 >;
 
 function AuthSignupScreen({navigation}: AuthScreenProps) {
+  const {signupMutation, loginMutation} = useAuth();
+  const passwordRef = useRef<TextInput | null>(null);
+  const passwordConfirmRef = useRef<TextInput | null>(null);
+  const nicknameRef = useRef<TextInput | null>(null);
+  const signup = useForm({
+    initialValue: {email: '', password: '', passwordConfirm: '', nickname: ''},
+    validate: validateSignup,
+  });
+
+  const handleSubmit = () => {
+    const {email, password} = signup.values;
+    signupMutation.mutate(
+      {
+        email,
+        password,
+        nickname: '',
+      },
+      {
+        onSuccess: () => loginMutation.mutate({email, password}),
+      },
+    );
+  };
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>AuthSignupScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          autoFocus
+          placeholder="이메일"
+          error={signup.errors.email}
+          touched={signup.touched.email}
+          inputMode="email"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          ref={passwordRef}
+          placeholder="비밀번호"
+          textContentType="oneTimeCode"
+          error={signup.errors.password}
+          touched={signup.touched.password}
+          secureTextEntry
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordConfirmRef.current?.focus()}
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          ref={passwordConfirmRef}
+          placeholder="비밀번호 확인"
+          error={signup.errors.passwordConfirm}
+          touched={signup.touched.passwordConfirm}
+          secureTextEntry
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+        <InputField
+          ref={nicknameRef}
+          placeholder="닉네임"
+          error={signup.errors.nickname}
+          touched={signup.touched.nickname}
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
+          {...signup.getTextInputProps('nickname')}
+        />
       </View>
+      <CustomButton label="회원가입" onPress={handleSubmit} />
     </SafeAreaView>
   );
 }
 
 export default AuthSignupScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
