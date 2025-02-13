268aa258f956711fb238f1044538be92d25f6db7
juhkang <juhyunk0820@naver.com>
2025-02-14 00:15:13 +0900

    validate 수정 - 닉네임 옵션 추가

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,12 +1,14 @@
 type UserInfomation = {
   email: string;
   password: string;
+  nickname?: string; //닉네임 정보 추가
 };
 
 function validateUser(values: UserInfomation) {
   const errors = {
     email: '',
     password: '',
+    nickname: '',
   };
 
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
@@ -23,7 +25,9 @@ function validateLogin(values: UserInfomation) {
   return validateUser(values);
 }
 
-function validateSignup(values: UserInfomation & {passwordConfirm: string}) {
+function validateSignup(
+  values: UserInfomation & {passwordConfirm: string; nickname: string},
+) {
   const errors = validateUser(values);
   const signupErrors = {...errors, passwordConfirm: ''};
 
@@ -31,7 +35,48 @@ function validateSignup(values: UserInfomation & {passwordConfirm: string}) {
     signupErrors.passwordConfirm = '비밀번호가 일치하지않습니다.';
   }
 
+  if (!values.nickname) {
+    signupErrors.nickname = '닉네임을 입력해주세요.';
+  }
+
   return signupErrors;
 }
+
+// const validateSignup = (values: {
+//   email: string;
+//   password: string;
+//   passwordConfirm: string;
+//   nickname: string;
+// }) => {
+//   const errors: {
+//     email: string;
+//     password: string;
+//     passwordConfirm: string;
+//     nickname: string;
+//   } = {
+//     email: '',
+//     password: '',
+//     passwordConfirm: '',
+//     nickname: '',
+//   };
+
+//   if (!values.email) {
+//     errors.email = '이메일을 입력해주세요.';
+//   }
+
+//   if (!values.password) {
+//     errors.password = '비밀번호를 입력해주세요.';
+//   }
+
+//   if (values.password !== values.passwordConfirm) {
+//     errors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+//   }
+
+//   if (!values.nickname) {
+//     errors.nickname = '닉네임을 입력해주세요.';
+//   }
+
+//   return errors;
+// };
 
 export {validateLogin, validateSignup};
