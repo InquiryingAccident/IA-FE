a577bd95f1e770185cba09154f8e0e9880c9603a
juhkang <juhyunk0820@naver.com>
2025-02-14 17:47:15 +0900

    RootNavigator - useAuth커스텀 훅 사용

--- src/navigations/root/RootNavigator.tsx
+++ src/navigations/root/RootNavigator.tsx
@@ -1,46 +1,12 @@
 import React, {useEffect} from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {useAuth} from '@/utils/AuthContext';
-import {Text} from 'react-native';
+import useAuth from '@/hooks/queries/useAuth';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
-import {getEncryptStorage} from '@/utils/EncryptStorage';
-import {storageKeys} from '@/constants/keys';
-import axiosInstance from '@/api/axios';
+import MainTabNavigator from '../tab/MainTabNavigator';
 
-const Stack = createStackNavigator();
+function RootNavigator() {
+  const {isLogin} = useAuth();
 
-const RootNavigator = () => {
-  const {isLogin, setIsLogin} = useAuth();
-
-  const refreshAuth = async () => {
-    const refreshToken = await getEncryptStorage(storageKeys.REFRESH_TOKEN);
-    if (!refreshToken) {
-      offLogin();
-      return;
-    }
-    const accessToken = await getEncryptStorage(storageKeys.ACCESS_TOKEN);
-    const sendFormData = new FormData();
-    sendFormData.append('refreshToken', refreshToken);
-    sendFormData.append('accessToken', accessToken);
-    const response = await axiosInstance.post('/auth/refresh', sendFormData);
-    if (response.status === 200) {
-      onLogin();
-    } else {
-      offLogin();
-    }
-  };
-  const onLogin = () => {
-    setIsLogin(true);
-  };
-  const offLogin = () => {
-    setIsLogin(false);
-  };
-
-  useEffect(() => {
-    refreshAuth();
-  }, []);
-
-  return isLogin ? <Text>로그인 성공</Text> : <AuthStackNavigator />;
-};
+  return <>{!isLogin ? <MainTabNavigator /> : <AuthStackNavigator />}</>;
+}
 
 export default RootNavigator;
