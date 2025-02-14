ed5308753c734b45d22446faf6586cced7ed59ca
juhkang <juhyunk0820@naver.com>
2025-02-15 04:42:22 +0900

    ParamList적용

--- src/screens/mainTab/tabSearchStack/TabSearchHomeScreen.tsx
+++ src/screens/mainTab/tabSearchStack/TabSearchHomeScreen.tsx
@@ -1,7 +1,15 @@
+import {tabSearchNavigations} from '@/constants';
+import {TabSearchStackParamList} from '@/navigations/stack/TabSearchStackNavigator';
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text} from 'react-native';
 
-function tabSearchHomeScreen() {
+type TabSearchScreenProps = StackScreenProps<
+  TabSearchStackParamList,
+  typeof tabSearchNavigations.TAB_SEARCH
+>;
+
+function tabSearchHomeScreen({navigation}: TabSearchScreenProps) {
   return (
     <View>
       <Text>항공편 검색, 선택사항으로 날짜, 등 입력 모달 추가</Text>
