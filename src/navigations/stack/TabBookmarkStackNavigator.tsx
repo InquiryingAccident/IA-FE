6b7363da9702f600899a4afb0745540ff42d45b1
juhkang <juhyunk0820@naver.com>
2025-02-15 04:43:37 +0900

    Bookmark 스택 구조 구현

---
+++ src/navigations/stack/TabBookmarkStackNavigator.tsx
@@ -0,0 +1,26 @@
+import {tabBookmarkNavigations} from '@/constants';
+import TabBookmarkHomeScreen from '@/screens/mainTab/tabBookmarkStack/TabBookmarkHomeScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+
+export type TabBookmarkStackParamList = {
+  [tabBookmarkNavigations.TAB_BOOKMARK]: undefined;
+};
+
+const Stack = createStackNavigator<TabBookmarkStackParamList>();
+
+function TabBookmarkStackNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName={tabBookmarkNavigations.TAB_BOOKMARK}
+      screenOptions={{
+        headerShown: false,
+        headerTitle: '',
+      }}>
+      <Stack.Screen
+        name={tabBookmarkNavigations.TAB_BOOKMARK}
+        component={TabBookmarkHomeScreen}
+      />
+    </Stack.Navigator>
+  );
+}
