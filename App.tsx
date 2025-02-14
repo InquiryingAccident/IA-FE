7dc5eb86baccc15c145c5c97a3f51f2a76bdd069
juhkang <juhyunk0820@naver.com>
2025-02-14 17:46:40 +0900

    query 사용

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
+import queryClient from '@/api/queryClient';
 import RootNavigator from '@/navigations/root/RootNavigator';
 import {AuthProvider} from '@/utils/AuthContext';
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator />
       </NavigationContainer>
-    </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
