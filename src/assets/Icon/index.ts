dc8e21d153a0b9ae7f9866fe26cb3c08ecca342a
juhkang <juhyunk0820@naver.com>
2025-02-17 22:40:28 +0900

    Icon Index수정 - User탭 추가

--- src/assets/Icon/index.ts
+++ src/assets/Icon/index.ts
@@ -4,7 +4,10 @@ export {default as Bookmark} from './Star.svg';
 export {default as HomeInactive} from './News-2.svg';
 export {default as SearchInactive} from './Search-2.svg';
 export {default as BookmarkInactive} from './Star-2.svg';
+export {default as UserInactive} from './User-2.svg';
 export {default as HomeActiveGreen} from './News-3.svg';
 export {default as SearchActiveGreen} from './Search-3.svg';
 export {default as BookmarkActiveGreen} from './Star-3.svg';
+export {default as UserActiveGreen} from './User.svg';
+
 // export {default as DeleteIcon} from './Delete.svg';
