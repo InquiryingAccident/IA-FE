5729c8393c743bfc1b039e3bb246f51197488b35
juhkang <juhyunk0820@naver.com>
2025-02-17 21:31:44 +0900

    login-signup 타입 다름 설정

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,14 +1,18 @@
-type UserInfomation = {
+type UserLoginInfomation = {
   email: string;
   password: string;
-  nickname?: string; //닉네임 정보 추가
 };
 
-function validateUser(values: UserInfomation) {
+type UserSignupInformation = {
+  email: string;
+  password: string;
+  nickname: string;
+};
+
+function validateLoginUser(values: UserLoginInfomation) {
   const errors = {
     email: '',
     password: '',
-    nickname: '',
   };
 
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
@@ -21,22 +25,38 @@ function validateUser(values: UserInfomation) {
   return errors;
 }
 
-function validateLogin(values: UserInfomation) {
-  return validateUser(values);
+function validateLogin(values: UserLoginInfomation) {
+  return validateLoginUser(values);
+}
+
+function validateSignupUser(values: UserSignupInformation) {
+  const errors = {
+    email: '',
+    password: '',
+    nickname: '',
+  };
+
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+    errors.email = '올바른 이메일 형식이 아닙니다.';
+  }
+  if (!(values.password.length >= 8 && values.password.length <= 20)) {
+    errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
+  }
+  if (!(values.nickname.length >= 2 && values.nickname.length <= 10)) {
+    errors.nickname = '닉네임은 2~10자 사이로 입력해주세요.';
+  }
+
+  return errors;
 }
 
 function validateSignup(
-  values: UserInfomation & {passwordConfirm: string; nickname: string},
+  values: UserSignupInformation & {passwordConfirm: string},
 ) {
-  const errors = validateUser(values);
+  const errors = validateSignupUser(values);
   const signupErrors = {...errors, passwordConfirm: ''};
 
   if (values.password !== values.passwordConfirm) {
     signupErrors.passwordConfirm = '비밀번호가 일치하지않습니다.';
-  }
-
-  if (!values.nickname) {
-    signupErrors.nickname = '닉네임을 입력해주세요.';
   }
 
   return signupErrors;
