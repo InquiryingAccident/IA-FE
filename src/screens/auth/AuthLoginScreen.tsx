66c2d40f51500762b8dc1af8ec170fcb415af1a9
juhkang <juhyunk0820@naver.com>
2025-02-17 21:32:11 +0900

    로그인 - custom hook 사용 변경

--- src/screens/auth/AuthLoginScreen.tsx
+++ src/screens/auth/AuthLoginScreen.tsx
@@ -1,88 +1,14 @@
-// import React, {useRef} from 'react';
-// import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
-// import {StackScreenProps} from '@react-navigation/stack';
-// import {AuthStackParamList} from '@/navigations/stack/AuthStackNavigator';
-// import {authNavigations} from '@/constants';
-// import useForm from '@/hooks/useForm';
-// import {validateLogin} from '@/utils';
-// import CustomButton from '@/components/custom/CustomButton';
-// import InputField from '@/components/custom/InputField';
-// import useAuth from '@/hooks/queries/useAuth';
-
-// type AuthScreenProps = StackScreenProps<
-//   AuthStackParamList,
-//   typeof authNavigations.AUTH_LOGIN
-// >;
-
-// function AuthLoginScreen({navigation}: AuthScreenProps) {
-//   const {isLogin} = useAuth();
-//   const passwordRef = useRef<TextInput | null>(null);
-//   const login = useForm({
-//     initialValue: {email: '', password: ''},
-//     validate: validateLogin,
-//   });
-
-//   const handleSubmit = () => {
-//     console.log('login.values', login.values);
-//     // loginMutation.mutate(login.values);
-//   };
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <View style={styles.inputContainer}>
-//         <InputField
-//           autoFocus
-//           placeholder="이메일"
-//           error={login.errors.email}
-//           touched={login.touched.email}
-//           inputMode="email"
-//           returnKeyType="next"
-//           blurOnSubmit={false}
-//           onSubmitEditing={() => passwordRef.current?.focus()}
-//           {...login.getTextInputProps('email')}
-//         />
-//         <InputField
-//           ref={passwordRef}
-//           placeholder="비밀번호"
-//           error={login.errors.password}
-//           touched={login.touched.password}
-//           secureTextEntry
-//           returnKeyType="join"
-//           onSubmitEditing={handleSubmit}
-//           {...login.getTextInputProps('password')}
-//         />
-//       </View>
-//       <CustomButton
-//         label="로그인"
-//         variant="filled"
-//         size="large"
-//         onPress={handleSubmit}
-//       />
-//     </SafeAreaView>
-//   );
-// }
-
-// export default AuthLoginScreen;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     margin: 30,
-//   },
-//   inputContainer: {
-//     gap: 20,
-//     marginBottom: 30,
-//   },
-// });
-import React, {useState, useRef} from 'react';
-import {SafeAreaView, StyleSheet, View, Alert, TextInput} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {AuthStackParamList} from '@/navigations/stack/AuthStackNavigator';
-import {authNavigations, storageKeys} from '@/constants';
+import {authNavigations, errorMessages} from '@/constants';
+import useForm from '@/hooks/useForm';
+import {validateLogin} from '@/utils';
 import CustomButton from '@/components/custom/CustomButton';
 import InputField from '@/components/custom/InputField';
-import axiosInstance from '@/api/axios';
-import {setEncryptStorage, useAuth} from '@/utils';
+import useAuth from '@/hooks/queries/useAuth';
+import Toast from 'react-native-toast-message';
 
 type AuthScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -90,58 +16,24 @@ type AuthScreenProps = StackScreenProps<
 >;
 
 function AuthLoginScreen({navigation}: AuthScreenProps) {
-  const {setIsLogin} = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState<{email?: string; password?: string}>({});
-  const [touched, setTouched] = useState<{email: boolean; password: boolean}>({
-    email: false,
-    password: false,
+  const {loginMutation} = useAuth();
+  const passwordRef = useRef<TextInput | null>(null);
+  const login = useForm({
+    initialValue: {email: '', password: ''},
+    validate: validateLogin,
   });
 
-  const passwordRef = useRef<TextInput | null>(null);
-
-  const handleSubmit = async () => {
-    let valid = true;
-    const newErrors: {email?: string; password?: string} = {};
-
-    if (!email.trim()) {
-      newErrors.email = '이메일을 입력해주세요';
-      valid = false;
-    }
-    if (!password) {
-      newErrors.password = '비밀번호를 입력해주세요';
-      valid = false;
-    }
-
-    setErrors(newErrors);
-
-    if (valid) {
-      try {
-        console.log('login values:', {email, password});
-        const formData = new FormData();
-        formData.append('email', email);
-        formData.append('password', password);
-
-        const response = await axiosInstance.post('/api/auth/login', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
+  const handleSubmit = () => {
+    loginMutation.mutate(login.values, {
+      onError: error => {
+        Toast.show({
+          type: 'error',
+          text1: error.response?.data.message || errorMessages.UNEXPECTED_ERROR,
+          position: 'bottom',
+          visibilityTime: 2000,
         });
-
-        if (response.status === 200) {
-          Alert.alert('로그인 성공');
-          const data = await response.data;
-          await setEncryptStorage(storageKeys.ACCESS_TOKEN, data.accessToken);
-          await setEncryptStorage(storageKeys.REFRESH_TOKEN, data.refreshToken);
-          setIsLogin(true);
-        } else {
-          Alert.alert('로그인 실패');
-        }
-      } catch (error) {
-        Alert.alert('로그인 실패');
-      }
-    }
+      },
+    });
   };
 
   return (
@@ -150,31 +42,23 @@ function AuthLoginScreen({navigation}: AuthScreenProps) {
         <InputField
           autoFocus
           placeholder="이메일"
-          error={errors.email}
-          touched={touched.email}
+          error={login.errors.email}
+          touched={login.touched.email}
           inputMode="email"
           returnKeyType="next"
-          blurOnSubmit={false}
+          // blurOnSubmit={false}
           onSubmitEditing={() => passwordRef.current?.focus()}
-          value={email}
-          onChangeText={text => {
-            setEmail(text);
-            setTouched(prev => ({...prev, email: true}));
-          }}
+          {...login.getTextInputProps('email')}
         />
         <InputField
           ref={passwordRef}
           placeholder="비밀번호"
-          error={errors.password}
-          touched={touched.password}
+          error={login.errors.password}
+          touched={login.touched.password}
           secureTextEntry
           returnKeyType="join"
           onSubmitEditing={handleSubmit}
-          value={password}
-          onChangeText={text => {
-            setPassword(text);
-            setTouched(prev => ({...prev, password: true}));
-          }}
+          {...login.getTextInputProps('password')}
         />
       </View>
       <CustomButton
