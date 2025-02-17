e103664d9b6dfeaac5a61e44a1d6cc815556a3d6
juhkang <juhyunk0820@naver.com>
2025-02-17 21:32:53 +0900

    CustomHook 사용 - 자동로그인 구현

--- src/navigations/root/RootNavigator.tsx
+++ src/navigations/root/RootNavigator.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect} from 'react';
-// import useAuth from '@/hooks/queries/useAuth';
-import {useAuth} from '@/utils';
+import useAuth from '@/hooks/queries/useAuth';
+// import {useAuth} from '@/utils';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
 import MainTabNavigator from '../tab/MainTabNavigator';
 
 function RootNavigator() {
   // const {isLogin} = useAuth();
-  const {isLogin, setIsLogin} = useAuth();
+  const {isLogin} = useAuth();
 
   return <>{isLogin ? <MainTabNavigator /> : <AuthStackNavigator />}</>;
 }
