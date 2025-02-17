4015547c8f4fd6b5b4ee7bee7face9e3256be44e
juhkang <juhyunk0820@naver.com>
2025-02-17 22:41:51 +0900

    User탭 네비게이터 설정

---
+++ src/navigations/stack/TabUserStackNavigator.tsx
@@ -0,0 +1,28 @@
+import {tabUserNavigations} from '@/constants';
+import TabUserHomeScreen from '@/screens/mainTab/tabUserStack/TabUserHomeScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+
+export type TabUserStackParamList = {
+  [tabUserNavigations.TAB_USER]: undefined;
+};
+
+const Stack = createStackNavigator<TabUserStackParamList>();
+
+function TabUserStackNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName={tabUserNavigations.TAB_USER}
+      screenOptions={{
+        headerShown: false,
+        headerTitle: '',
+      }}>
+      <Stack.Screen
+        name={tabUserNavigations.TAB_USER}
+        component={TabUserHomeScreen}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default TabUserStackNavigator;
