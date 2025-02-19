8fd63d1be03a63a0b24b944c73a07ae61f4b1cbc
juhkang <juhyunk0820@naver.com>
2025-02-19 22:45:31 +0900

    ErrorMessage Update - validate

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -16,6 +16,10 @@ const alerts = {
 const errorMessages = {
   UNEXPECTED_ERROR: '알 수 없는 에러가 발생했습니다.',
   CANNOT_GET_ADDRESS: '주소를 알 수 없습니다.',
+  VALIDATE_EMAIL: '올바른 이메일 형식이 아닙니다.',
+  VALIDATE_PASSWORD: '비밀번호는 8~20자 사이로 입력해주세요.',
+  VALIDATE_PASSWORDCONFIRM: '비밀번호가 일치하지않습니다.',
+  VALIDATE_NICKNAME: '닉네임은 2~10자 사이로 입력해주세요.',
 };
 
 export {alerts, errorMessages};
