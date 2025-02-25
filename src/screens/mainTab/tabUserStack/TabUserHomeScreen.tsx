109936a13ef8f56dc7c369cccb3ca55138ff3be2
juhkang <juhyunk0820@naver.com>
2025-02-25 19:54:42 +0900

    로그아웃회원탈퇴 UI 구현 : fix : 로그아웃UI 추가 https://github.com/InquiryingAccident/IA-FE/issues/15

--- src/screens/mainTab/tabUserStack/TabUserHomeScreen.tsx
+++ src/screens/mainTab/tabUserStack/TabUserHomeScreen.tsx
@@ -1,7 +1,9 @@
-import {tabUserNavigations} from '@/constants';
+import SettingItem from '@/components/setting/SettingItem';
+import {colors, tabUserNavigations} from '@/constants';
+import useAuth from '@/hooks/queries/useAuth';
 import {TabUserStackParamList} from '@/navigations/stack/TabUserStackNavigator';
 import {StackScreenProps} from '@react-navigation/stack';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 type TabUserScreenProps = StackScreenProps<
   TabUserStackParamList,
@@ -9,11 +11,29 @@ type TabUserScreenProps = StackScreenProps<
 >;
 
 function TabUserHomeScreen({navigation}: TabUserScreenProps) {
+  const {logoutMutation} = useAuth();
+
+  const deleteUser = async () => {
+    console.log('delte Token: Logout or DeleteUser');
+    logoutMutation.mutate(null);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>UserInfo</Text>
+    <SafeAreaView style={styles.container}>
+      <SettingItem
+        title="로그아웃"
+        onPress={deleteUser}
+        color={colors.RED_500}
+      />
     </SafeAreaView>
   );
 }
 
 export default TabUserHomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
