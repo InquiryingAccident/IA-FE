91e108f79b2df0ae0238911671485fd2660e0a26
juhkang <juhyunk0820@naver.com>
2025-03-10 17:15:44 +0900

    전역 상태관리를 통한 데이터 저장 : fix : 저장된 정보 활용 https://github.com/InquiryingAccident/IA-FE/issues/27

--- src/screens/mainTab/tabSearchStack/TabSearchIdentScreen.tsx
+++ src/screens/mainTab/tabSearchStack/TabSearchIdentScreen.tsx
@@ -1,5 +1,6 @@
 import {tabSearchNavigations} from '@/constants';
 import {TabSearchStackParamList} from '@/navigations/stack/TabSearchStackNavigator';
+import {useFlightsStore} from '@/store/flightsStore';
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
@@ -10,16 +11,30 @@ type TabSearchScreenProps = StackScreenProps<
 >;
 
 function TabSearchIdentScreen({navigation}: TabSearchScreenProps) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.identInfo}>
-        <Text>
-          항공편 스크린, 출발지 - 목적지, 그 아래에 출발시간-예상도착시간 등의
-          정보 입력
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
+  const flights = useFlightsStore(state => state.flights);
+  if (flights?.length == 0) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.identInfo}>
+          <Text>항공편 정보가 없습니다.</Text>
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.identInfo}>
+          <Text>
+            항공편 스크린, 출발지 - 목적지, 그 아래에 출발시간-예상도착시간 등의
+            정보 입력
+          </Text>
+          {flights && flights.length > 0 && (
+            <Text>{flights[0].origin.name}</Text>
+          )}
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default TabSearchIdentScreen;
