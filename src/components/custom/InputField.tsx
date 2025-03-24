b9cc119038e161aab757d06da5710f924da5d569
juhkang <juhyunk0820@naver.com>
2025-03-24 20:02:09 +0900

    로그인 및 회원가입 리팩토링 : fix : 입력 컴포넌트 변경 - 문구 적용 및 상황에 따른 UI변경 https://github.com/InquiryingAccident/IA-FE/issues/29

--- src/components/custom/InputField.tsx
+++ src/components/custom/InputField.tsx
@@ -13,31 +13,48 @@ import {mergeRefs} from '@/utils';
 import {colors} from '@/constants';
 
 interface InputFieldProps extends TextInputProps {
+  title?: string;
   disabled?: boolean;
   error?: string;
   touched?: boolean;
+  message?: string;
+  check?: boolean;
+  available?: boolean;
+  checkedButton?: () => void;
 }
 
 const deviceHeight = Dimensions.get('screen').height;
 
 const InputField = forwardRef(
   (
-    {disabled = false, error, touched, ...props}: InputFieldProps,
+    {
+      title,
+      disabled = false,
+      error,
+      touched,
+      message,
+      check = false,
+      available = false,
+      checkedButton,
+      ...props
+    }: InputFieldProps,
     ref?: ForwardedRef<TextInput>,
   ) => {
     const innerRef = useRef<TextInput | null>(null);
-
     const handlePressInput = () => {
       innerRef.current?.focus();
     };
 
     return (
       <Pressable onPress={handlePressInput}>
+        <Text style={styles.title}>{title}</Text>
         <View
           style={[
             styles.container,
             disabled && styles.disabled,
             touched && Boolean(error) && styles.inputError,
+            touched && !error && check && !available && styles.inputError,
+            touched && !error && check && available && styles.inputAvailable,
           ]}>
           <TextInput
             ref={ref ? mergeRefs(innerRef, ref) : innerRef}
@@ -49,10 +66,22 @@ const InputField = forwardRef(
             autoCorrect={false}
             {...props}
           />
-          {touched && Boolean(error) && (
-            <Text style={styles.error}>{error}</Text>
+          {check && (
+            <Pressable style={styles.checkedButton} onPress={checkedButton}>
+              <Text style={styles.checkedButtonText}>중복확인</Text>
+            </Pressable>
           )}
         </View>
+        {touched && Boolean(error) && <Text style={styles.error}>{error}</Text>}
+        {!Boolean(error) && (
+          <Text
+            style={[
+              styles.unError,
+              available ? styles.availableText : styles.notAvailableText,
+            ]}>
+            {message}
+          </Text>
+        )}
       </Pressable>
     );
   },
@@ -60,27 +89,64 @@ const InputField = forwardRef(
 
 const styles = StyleSheet.create({
   container: {
+    borderRadius: 8,
     borderWidth: 1,
     borderColor: colors.GRAY_200,
     padding: deviceHeight > 700 ? 15 : 10,
+    height: deviceHeight > 700 ? 56 : 50,
+    flexDirection: 'row',
+  },
+  title: {
+    marginBottom: 12,
+    fontSize: 16,
+    left: 4,
   },
   input: {
     fontSize: 16,
     color: colors.BLACK,
     padding: 0,
+    flex: 0.8,
   },
   disabled: {
     backgroundColor: colors.GRAY_200,
     color: colors.GRAY_700,
   },
+  checkedButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.BLUE_BASIC,
+    borderRadius: 4,
+    flex: 0.2,
+  },
+  checkedButtonText: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: colors.WHITE,
+  },
   inputError: {
-    borderWidth: 1,
     borderColor: colors.RED_300,
+    borderWidth: 1,
+  },
+  inputAvailable: {
+    borderColor: colors.BLUE_BASIC,
+    borderWidth: 1,
   },
   error: {
+    left: 4,
     color: colors.RED_500,
     fontSize: 12,
-    paddingTop: 5,
+    marginTop: 10,
+  },
+  unError: {
+    left: 4,
+    fontSize: 12,
+    marginTop: 10,
+  },
+  availableText: {
+    color: colors.BLUE_BASIC,
+  },
+  notAvailableText: {
+    color: colors.RED_500,
   },
 });
 
