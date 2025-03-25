f436210a0d3fdc0d5369780a13ea1bda69fc9d17
juhkang <juhyunk0820@naver.com>
2025-03-25 22:52:00 +0900

    입력 시 키보드에 의해 가려지는 현상 개선 : fix : KeyboardAvoid적용: 로그인화면 https://github.com/InquiryingAccident/IA-FE/issues/30

--- src/screens/auth/AuthLoginScreen.tsx
+++ src/screens/auth/AuthLoginScreen.tsx
@@ -3,9 +3,10 @@ import {
   Alert,
   Dimensions,
   Image,
+  KeyboardAvoidingView,
+  Platform,
   SafeAreaView,
   StyleSheet,
-  Text,
   TextInput,
   View,
 } from 'react-native';
@@ -18,6 +19,7 @@ import CustomButton from '@/components/custom/CustomButton';
 import InputField from '@/components/custom/InputField';
 import useAuth from '@/hooks/queries/useAuth';
 import Toast from 'react-native-toast-message';
+import KeyboardAvoid from '@/utils/KeyboardAvoid';
 
 type AuthScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -54,45 +56,47 @@ function AuthLoginScreen({navigation}: AuthScreenProps) {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Image
-          source={require('@/assets/logo/Logo.png')}
-          style={styles.image}
+    <KeyboardAvoid>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image
+            source={require('@/assets/logo/Logo.png')}
+            style={styles.image}
+          />
+          <InputField
+            title="아이디"
+            autoFocus
+            placeholder="이메일"
+            error={login.errors.email}
+            touched={login.touched.email}
+            inputMode="email"
+            returnKeyType="next"
+            // message="올바른 이메일 형식입니다."
+            // blurOnSubmit={false}
+            onSubmitEditing={() => passwordRef.current?.focus()}
+            {...login.getTextInputProps('email')}
+          />
+          <InputField
+            title="비밀번호"
+            ref={passwordRef}
+            placeholder="비밀번호"
+            error={login.errors.password}
+            touched={login.touched.password}
+            secureTextEntry
+            returnKeyType="join"
+            onSubmitEditing={handleSubmit}
+            {...login.getTextInputProps('password')}
+          />
+        </View>
+        <CustomButton
+          label="로그인"
+          variant="filled"
+          size="large"
+          inValid={!login.values.password}
+          onPress={handleSubmit}
         />
-        <InputField
-          title="아이디"
-          autoFocus
-          placeholder="이메일"
-          error={login.errors.email}
-          touched={login.touched.email}
-          inputMode="email"
-          returnKeyType="next"
-          // message="올바른 이메일 형식입니다."
-          // blurOnSubmit={false}
-          onSubmitEditing={() => passwordRef.current?.focus()}
-          {...login.getTextInputProps('email')}
-        />
-        <InputField
-          title="비밀번호"
-          ref={passwordRef}
-          placeholder="비밀번호"
-          error={login.errors.password}
-          touched={login.touched.password}
-          secureTextEntry
-          returnKeyType="join"
-          onSubmitEditing={handleSubmit}
-          {...login.getTextInputProps('password')}
-        />
-      </View>
-      <CustomButton
-        label="로그인"
-        variant="filled"
-        size="large"
-        inValid={!login.values.password}
-        onPress={handleSubmit}
-      />
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoid>
   );
 }
 
