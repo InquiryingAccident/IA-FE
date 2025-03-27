2b731cab06598cddccc6db166c61af3b4a270244
juhkang <juhyunk0820@naver.com>
2025-03-27 13:54:23 +0900

    queryKey 상수 수정

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -2,6 +2,8 @@ const queryKeys = {
   AUTH: 'auth',
   GET_ACCESS_TOKEN: 'getAccessToken',
   GET_PROFILE: 'getProfile',
+  GET_FLIGHT: 'getFlight',
+  GET_MODEL: 'getModel',
 } as const;
 
 const storageKeys = {
