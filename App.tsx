a1d12ed2059352f827f8b1aa68c5bd77268e6e19
juhkang <juhyunk0820@naver.com>
2025-04-10 22:01:24 +0900

    UI및 기능 개선 : feature : ToastMessage 사용- 직관적이며 시각적으로 좋은 알림 기능 https://github.com/InquiryingAccident/IA-FE/issues/33

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import queryClient from '@/api/queryClient';
+import {colors} from '@/constants';
 import RootNavigator from '@/navigations/root/RootNavigator';
 import {AuthProvider} from '@/utils/AuthContext';
 import {NavigationContainer} from '@react-navigation/native';
@@ -6,6 +7,39 @@ import {QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {KeyboardProvider} from 'react-native-keyboard-controller';
+import Toast, {
+  BaseToast,
+  BaseToastProps,
+  ErrorToast,
+} from 'react-native-toast-message';
+
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: colors.BLUE_BASIC}}
+      contentContainerStyle={{paddingHorizontal: 15}}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+  error: (props: BaseToastProps) => (
+    <ErrorToast
+      {...props}
+      style={{borderLeftColor: colors.RED_500}}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+};
 
 function App(): React.JSX.Element {
   return (
@@ -13,6 +47,7 @@ function App(): React.JSX.Element {
       <QueryClientProvider client={queryClient}>
         <NavigationContainer>
           <RootNavigator />
+          <Toast config={toastConfig} />
         </NavigationContainer>
       </QueryClientProvider>
     </KeyboardProvider>
