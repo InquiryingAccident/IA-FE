f5a0826a23d58275972a5f2b5e8dc7b7c0e13004
juhkang <juhyunk0820@naver.com>
2025-04-10 20:05:38 +0900

    EditInfo HeaderRight 컴포넌트 삭제

--- src/components/user/EditInfoHeaderRight.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HeaderButton from '@/components/custom/HeaderButton';
-
-function EditInfoHeaderRight(onSubmit: () => void) {
-  return <HeaderButton labelText="완료" onPress={onSubmit} />;
-}
-
-export default EditInfoHeaderRight;
