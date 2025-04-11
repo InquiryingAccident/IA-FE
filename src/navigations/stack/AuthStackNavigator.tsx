9146ea40f5738e58b7a2b039f5f80c9e514419a4
juhkang <juhyunk0820@naver.com>
2025-04-11 20:13:54 +0900

    소셜 로그인 기능 추가애플 카카오 : fix : 카카오 로그인 페이지에 대한 오타 수정 https://github.com/InquiryingAccident/IA-FE/issues/35

--- src/navigations/stack/AuthStackNavigator.tsx
+++ src/navigations/stack/AuthStackNavigator.tsx
@@ -4,11 +4,13 @@ import AuthHomeScreen from '@/screens/auth/AuthHomeScreen';
 import AuthLoginScreen from '@/screens/auth/AuthLoginScreen';
 import AuthSignupScreen from '@/screens/auth/AuthSignupScreen';
 import {authNavigations} from '@/constants';
+import AuthKakaoLoginScreen from '@/screens/auth/AuthKakaoLoginScreen';
 
 export type AuthStackParamList = {
   [authNavigations.AUTH_HOME]: undefined;
   [authNavigations.AUTH_LOGIN]: undefined;
   [authNavigations.AUTH_SIGNUP]: undefined;
+  [authNavigations.AUTH_KAKAO]: undefined;
 };
 
 const Stack = createStackNavigator<AuthStackParamList>();
@@ -50,6 +52,13 @@ function AuthStackNavigator() {
         component={AuthSignupScreen}
         options={{
           headerTitle: '회원가입',
+        }}
+      />
+      <Stack.Screen
+        name={authNavigations.AUTH_KAKAO}
+        component={AuthKakaoLoginScreen}
+        options={{
+          headerTitle: '카카오 로그인',
         }}
       />
     </Stack.Navigator>
